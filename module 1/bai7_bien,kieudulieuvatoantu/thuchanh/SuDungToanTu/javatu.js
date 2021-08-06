50cf5c578055fddf5b3e3b04152e23ca31df3df9
truongthimyvan0101 <truongthimyvan9@gmail.com>
2021-08-06 16:32:11 +0700

    up

---
+++ module 1/bai7_bien,kieudulieuvatoantu/thuchanh/SuDungToanTu/javatu.js
@@ -0,0 +1,6 @@
+var width = prompt("Enter the width");
+var height = prompt ("Enter the height");
+width = parseInt( width);
+height = parseInt(height);
+var area = width * height;
+document.write("The area is:"+ area);
