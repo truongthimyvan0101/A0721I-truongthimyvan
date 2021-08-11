722dae408e15aaafbd2e7e1e30ae64d4d7b06387
truongthimyvan0101 <truongthimyvan9@gmail.com>
2021-08-11 18:50:02 +0700

    up

---
+++ module 1/bai9_cautrucdieukien2/baitap/TinhChiSoCanNangCoThe/cannang.js
@@ -0,0 +1,8 @@
+var height1 = document.getElementById('height');
+var weight1 = document.getElementById('weight');
+var cal = document.getElementById('cal');
+
+function calculate(height, weight) {
+    if (height <= 0 || isNaw)
+
+}
